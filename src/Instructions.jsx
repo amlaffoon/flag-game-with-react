e9211d6a8eccb1d9e982c9390204508dded10014
Anna Laffoon <ree11194@gmail.com>
2022-08-24 16:03:42 -0400

    initial commit

---
+++ src/Instructions.jsx
@@ -0,0 +1,17 @@
+function Instructions() {
+    return <div id="intro" class="box">
+        <p>
+            Identify whether or not the emoji is a flag. If you guess all of them correctly, you win!
+        </p>
+        <p>
+            You can
+            use
+            <kbd>Y</kbd> or <kbd>N</kbd> keys to answer.
+        </p>
+        <p>
+            Timer starts when you answer the first question.
+        </p>
+    </div>
+}
+
+export default Instructions;
